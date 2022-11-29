f73aff5ca74b78b08c515d39677975d1f06e4a55
ZhenlongMa <61593179+ZhenlongMa@users.noreply.github.com>
2022-11-29 20:34:40 +0800

    return

--- sv/test/test_direct_param.sv
+++ sv/test/test_direct_param.sv
@@ -540,10 +540,6 @@ class test_direct_param extends uvm_test;
         remote_qp = qp.remote_qp;
         // remote_qpn = remote_qp.ctx.local_qpn;
 
-        // host 1 is not sender
-        if (host_id == 1) begin
-            break;
-        end
 
         if (host_id == 0) begin
             remote_host_id = 1;
